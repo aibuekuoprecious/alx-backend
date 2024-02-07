4c034489476e84317e640a13f7103fd240764c36
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-02-07 11:43:31 +0100

    Internationalization

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+A Basic flask application
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """
+    Application configuration class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+def create_app() -> Flask:
+    """
+    Creates and configures the Flask application
+
+    Returns:
+        Flask: The configured Flask application
+    """
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    Babel(app)
+
+    @app.route('/', strict_slashes=False)
+    def index() -> str:
+        """
+        Renders a basic html template
+
+        Returns:
+            str: The rendered HTML template
+        """
+        return render_template('1-index.html')
+
+    return app
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
---
+++ 0x02-i18n/README.md
@@ -0,0 +1,7 @@
+# ALX Backend - Internationalization (i18n)
+
+This repository demonstrates the following concepts:
+
+- Parametrizing Flask templates to display different languages
+- Inferring the correct locale based on URL parameters, user settings, or request headers
+- Localizing timestamps
